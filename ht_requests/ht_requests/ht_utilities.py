6e1ece55d880337c5e530208c269dda8f339024b
Joey Kleingers <joey.kleingers@bluequartz.net>
2021-04-08 15:57:56 -0400

    Updating unfinished method with instructions on how to write it.
    HyperThought only takes metadata in a list of json objects with a specific schema.
    This method should convert the metadata rows in the table into a list of json objects like shown in the example comment.
    
    Signed-off-by: Joey Kleingers <joey.kleingers@bluequartz.net>

--- ht_requests/ht_requests/ht_utilities.py
+++ ht_requests/ht_requests/ht_utilities.py
@@ -18,5 +18,30 @@ def dict_to_ht_metadata(metadata_dict):
         The list of metadata json objects usable by HyperThought endpoints.
     """
 
-    for key, value in metadata_dict.items():
-        print(key)
+    # This method still needs to be written!
+
+    # The dictionary of metadata table entries needs to be converted into json that HyperThought can use.
+
+    # Example HyperThought metadata list:
+    # [
+    #     {
+    #         'keyName': 'Key1',
+    #         'value': {
+    #             'type': 'string',
+    #             'link': "I am the first key."
+    #         },
+    #         'unit': None,
+    #         'annotation': "http://www.google.com"
+    #     },
+    #     {
+    #         'keyName': 'RandomNumber',
+    #         'value': {
+    #             'type': 'string',     # Even though this data is an integer, its type for HyperThought purposes should be set to string
+    #             'link': 291001
+    #         },
+    #         'unit': None,
+    #         'annotation': 'A random number'
+    #     }
+    # ]
+
+    pass
