2c909ffd59965f6154ac2ea60d6d908a7fe0373d
Michael Jackson <mike.jackson@bluequartz.net>
2021-06-17 11:34:09 -0400

    Fix uploading that was broke a few commits back.
    Perform a quick sanity check to ensure all files exist
    
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- uploader.py
+++ uploader.py
@@ -1,4 +1,4 @@
-from PySide2.QtCore import QObject, Signal
+from PySide2.QtCore import QFileInfo, QObject, Signal
 from ht_requests.ht_requests import ht_utilities
 from ht_requests.ht_requests import htauthcontroller
 from ht_requests.ht_requests import ht_requests
@@ -9,29 +9,37 @@ class Uploader(QObject):
     currentUploadDone = Signal(int)
     allUploadsDone = Signal()
     currentlyUploading = Signal(str)
-    def __init__(self, parent = None):
+
+    def __init__(self, parent=None):
         super(Uploader, self).__init__(parent)
         self.interrupt = False
 
-
-
-
-    def performUpload(self,metadataList ,authControl, ht_id_path, metadata):
+    def performUpload(self, metadataList, authControl, ht_id_path, metadata):
         self.interrupt = False
-        for i in tqdm(range(len(metadataList)), desc = "Uploading Files"):
-            if not QFile.exists(metadataList[i]):
-                print("file no longer exists")
-                break
 
+        # Check that all files initially exist. This is a quick sanity check. The user could
+        # *still* delete a file out from under the codes.
+        for i in tqdm(range(len(metadataList)), desc="Checking Files"):
+            if not QFileInfo.exists(metadataList[i]):
+                self.currentlyUploading.emit(
+                    "File Missing: " + metadataList[i].split("/")[-1])
+                self.interrupt = True
+
+        for i in tqdm(range(len(metadataList)), desc="Uploading Files"):
             if self.interrupt:
                 break
-            self.currentlyUploading.emit("Currently uploading: "+ metadataList[i].split("/")[-1])
-            ht_requests.upload_file(authControl, metadataList[i], 'user', None, ht_id_path, metadata)
-            self.currentUploadDone.emit(i+1)
+
+            if not QFileInfo.exists(metadataList[i]):
+                self.currentlyUploading.emit(
+                    "File Missing: " + metadataList[i].split("/")[-1])
+                break
+            else:
+                self.currentlyUploading.emit(
+                    "Currently uploading: " + metadataList[i].split("/")[-1])
+                ht_requests.upload_file(
+                    authControl, metadataList[i], 'user', None, ht_id_path, metadata)
+                self.currentUploadDone.emit(i+1)
         self.allUploadsDone.emit()
 
     def interruptUpload(self):
         self.interrupt = True
-
-
-
