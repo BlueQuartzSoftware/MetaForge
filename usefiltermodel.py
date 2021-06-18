81f33ca9f95ca9422f9f9f403237d4e89c11ebfc
rcharlesics <rcharles@ics.com>
2021-06-18 12:03:00 -0400

    Bug Fix #12 Removing rows (name/value) then uploading is still uploading removed row Solution: Added a remove in the display table that is uploaded to HT
    Fixed error whenever templates were changed that the metadatalist was
    out of ranged

--- usefiltermodel.py
+++ usefiltermodel.py
@@ -7,18 +7,19 @@ class FilterModelU(QSortFilterProxyModel):
         self.fileType=[]
 
     def filterAcceptsRow(self,source_row, source_parent):
-        curSource = self.sourceModel().metadataList[source_row]['Source']
-        if "Custom Input" not in curSource:
-            splitSource= "/".join(curSource.split("/")[1:])
-            if splitSource in self.sourceModel().templatesources:
+        if len(self.sourceModel().metadataList) > source_row:
+            curSource = self.sourceModel().metadataList[source_row]['Source']
+            if "Custom Input" not in curSource:
+                splitSource= "/".join(curSource.split("/")[1:])
+                if splitSource in self.sourceModel().templatesources:
+                    if self.sourceModel().metadataList[source_row] not in self.displayed:
+                        self.displayed.append(self.sourceModel().metadataList[source_row])
+                    return True
+                else:
+                    return False
+            else:
                 if self.sourceModel().metadataList[source_row] not in self.displayed:
                     self.displayed.append(self.sourceModel().metadataList[source_row])
-                return True
-            else:
-                return False
-        else:
-            if self.sourceModel().metadataList[source_row] not in self.displayed:
-                self.displayed.append(self.sourceModel().metadataList[source_row])
 
-            return "Custom Input" in self.sourceModel().metadataList[source_row]['Source']
+                return "Custom Input" in self.sourceModel().metadataList[source_row]['Source']
 
