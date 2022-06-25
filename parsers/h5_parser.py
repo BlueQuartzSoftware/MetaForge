3819f19712b0120b7012c46749a783c939e360b4
Marcus D. Hanwell <marcus@quantumista.com>
2022-06-25 14:26:40 -0400

    Added a simple, first pass at a HDF5 parser
    This visits all entries in a HDF5 file, is the entry is a data set it
    then does some simple dispatch, and displays the simple single value
    entries. It needs extending to handle small arrays, and attributes.
    
    Signed-off-by: Marcus D. Hanwell <marcus@quantumista.com>

---
+++ parsers/h5_parser.py
@@ -0,0 +1,91 @@
+from parsers.metaforgeparser import MetaForgeParser
+from typing import List
+from pathlib import Path
+from uuid import UUID
+
+import os
+import h5py
+import numpy
+
+from parsers.metaforgeparser import MetaForgeParser
+
+class H5Parser(MetaForgeParser):
+  def __init__(self) -> None:
+    self.ext_list: list = ('.h5', '.oh5', '.emd')
+    self.count: int = 0
+    self.file_dict: object = {}
+
+  def human_label(self) -> str:
+    return "HDF5 Parser"
+
+  def version(self) -> str:
+    return '1.0'
+
+  def uuid(self) -> UUID:
+    return UUID('{7420a76b-f0de-4fb1-a932-b6cb969f7af6}')
+
+  def supported_file_extensions(self) -> list:
+    return self.ext_list
+  
+  def accepts_extension(self, extension: str) -> bool:
+    if extension in self.ext_list:
+      return True
+    return False
+
+  def type_dispatch(self, value: any) -> str|None:
+    """
+    Perform type dispatch and figure out whether to make an entry
+    """
+    if isinstance(value, bytes):
+      return value.decode('utf-8')
+    elif isinstance(value, numpy.void):
+      # These are some weirdly encoded numpy arrays!
+      return None
+    elif isinstance(value, (numpy.ndarray, numpy.generic)):
+      return str(value)
+    
+    print(f'value confused us: {value} {type(value)}')
+    return None
+
+  def visit_entry(self, name: str) -> None:
+    data = self.file[name]
+    if isinstance(data, h5py.Dataset):
+      if data.len() == 1:
+        # Handle the simple cases.
+        value = self.type_dispatch(data[0])
+        if value is not None:
+          self.file_dict[name] = value
+      else:
+        # This is where we should handle small sets.
+        print(f'{name} -> {type(data)} -> {data.len()}') 
+      self.count += 1
+
+  def parse_header_as_dict(self, filepath: Path) -> dict:
+    """
+    Description:
+
+    Parameters
+    ----------
+    filepath
+        The path to the HDF5 file to be parsed
+
+    Returns
+    -------
+    Dictionary
+        A dictionary containing the header information
+    
+    Example
+    -------
+    ```
+    parser = H5Parser()
+    parser.parse_header_as_dict('/some/path/to/a/file.h5')
+    ```
+    """
+    # Check our file exists before trying to open it!
+    if os.path.isfile(filepath):
+      print("File found!")
+      self.file = h5py.File(filepath, 'r')
+      self.file.visit(self.visit_entry)
+
+    return self.file_dict
+
