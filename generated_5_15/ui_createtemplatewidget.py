3bc3230181c7fd34dc0964ec48c26f25200e8bed
Joey Kleingers <joey.kleingers@bluequartz.net>
2022-07-13 14:13:39 -0400

    Updating Qt version in generated UI files.
    Signed-off-by: Joey Kleingers <joey.kleingers@bluequartz.net>

--- generated_5_15/ui_createtemplatewidget.py
+++ generated_5_15/ui_createtemplatewidget.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'createtemplatewidget.ui'
 ##
-## Created by: Qt User Interface Compiler version 5.15.2
+## Created by: Qt User Interface Compiler version 5.15.4
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
